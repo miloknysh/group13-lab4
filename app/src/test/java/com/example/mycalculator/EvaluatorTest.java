8600c31ac62d9c14d2a247972ef5904517072105
miloknysh <mknys040@uottawa.ca>
2025-11-10 11:31:51 -0500

    added tests for multiplication and division.

--- app/src/test/java/com/example/mycalculator/EvaluatorTest.java
+++ app/src/test/java/com/example/mycalculator/EvaluatorTest.java
@@ -11,15 +11,16 @@ import static org.junit.Assert.*;
  */
 public class EvaluatorTest {
 
+    private String actual;
+    private String expected;
+    private Evaluator eval = new Evaluator();
+
+
     @Test
     public void evaluateAddition() throws Exception {
 
-        Evaluator eval = new Evaluator();
-
-        //tests that should pass
-
-        String actual = eval.evaluate("2.7+3.4");
-        String expected = "6.10";
+        actual = eval.evaluate("2.7+3.4");
+        expected = "6.10";
         assertEquals(expected, actual);
 
         actual = eval.evaluate("50.43+10.57");
@@ -41,13 +42,17 @@ public class EvaluatorTest {
         actual = eval.evaluate("-74+-3");
         expected = "-77";
         assertEquals(expected, actual);
+    }
 
-        //tests that should fail
-
+    @Test
+    public void evaluateAdditionFail1() throws Exception{
         actual = eval.evaluate("2.5+3.5");
         expected = "6.0";
         assertEquals(expected, actual);
+    }
 
+    @Test
+    public void evaluateAdditionFail2() throws Exception{
         actual = eval.evaluate("1.1+3.2");
         expected = "4.3";
         assertEquals(expected, actual);
@@ -56,13 +61,8 @@ public class EvaluatorTest {
 
     @Test
     public void evaluateSubtraction() throws Exception {
-
-        Evaluator eval = new Evaluator();
-
-        //tests that should pass
-
-        String actual = eval.evaluate("2.7-3.4");
-        String expected = "-0.70";
+        actual = eval.evaluate("2.7-3.4");
+        expected = "-0.70";
         assertEquals(expected, actual);
 
         actual = eval.evaluate("50.43-10.57");
@@ -77,18 +77,80 @@ public class EvaluatorTest {
         expected = "-81";
         assertEquals(expected, actual);
 
-        actual = eval.evaluate("31.9-6.38");
-        expected = "25.52";
+        actual = eval.evaluate("-4.7-2.3");
+        expected = "-7";
         assertEquals(expected, actual);
 
-        actual = eval.evaluate("-74-3");
-        expected = "-77";
-        assertEquals(expected, actual);
+    }
 
-        //test that should fail
-
+    @Test
+    public void evaluateSubtractionFail1() throws Exception{
         actual = eval.evaluate("2.5-3.5");
         expected = "-1.0";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evaluateSubtractionFail2() throws Exception{
+        actual = eval.evaluate("5-3.001");
+        expected = "1.999";
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void evaluateMultiplication() throws Exception{
+        actual = eval.evaluate("5.2×3");
+        expected = "15.60";
+        assertEquals(expected, actual);
+
+        actual = eval.evaluate("-9×2");
+        expected = "-18";
+        assertEquals(expected, actual);
+
+        actual = eval.evaluate("0.25×76");
+        expected = "19";
+        assertEquals(expected, actual);
+
+        actual = eval.evaluate("17.8×0");
+        expected = "0";
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void evaluateMultiplicationFail1() throws Exception{
+        actual = eval.evaluate("5.01×3.8");
+        expected = "19.038";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evaluateMultiplicationFail2() throws Exception{
+        actual = eval.evaluate("7.5×2.4");
+        expected = "18.00";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evaluateDivision() throws Exception{
+        actual = eval.evaluate("0÷475");
+        expected = "0";
+        assertEquals(expected, actual);
+
+        actual = eval.evaluate("90÷0.1");
+        expected = "900";
+        assertEquals(expected, actual);
+
+        actual = eval.evaluate("-15.4÷3.2");
+        expected = "-4.81";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evaluateDivisionFail1() throws Exception{
+        actual = eval.evaluate("81.00÷1");
+        expected = "81.00";
         assertEquals(expected, actual);
     }
 
