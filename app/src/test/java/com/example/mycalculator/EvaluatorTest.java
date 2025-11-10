49cb85c61184e352fa91f0128cb7a8000e32bdd7
djone033-debug <djone033@uottawa.ca>
2025-11-09 22:34:24 -0500

    oops again

--- app/src/test/java/com/example/mycalculator/EvaluatorTest.java
+++ app/src/test/java/com/example/mycalculator/EvaluatorTest.java
@@ -88,7 +88,7 @@ public class EvaluatorTest {
         //test that should fail
 
         actual = eval.evaluate("2.5-3.5");
-        expected = "1.0";
+        expected = "-1.0";
         assertEquals(expected, actual);
     }
 
